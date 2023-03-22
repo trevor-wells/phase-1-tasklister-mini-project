8283b8c28ebe7c92b58b60a60750da0a65b4254f
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-03-22 16:32:44 -0600

    final commit. that was tough

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    const myToDo = document.querySelector("#new-task-description").value
+    console.log(myToDo)
+    buildToDo(myToDo)
+    form.reset()
+  })
 });
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click' , handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo}  `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
